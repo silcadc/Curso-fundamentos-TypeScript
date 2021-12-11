ea470636b73ca17ffe2c78c6ccc04367292d5961
Silena Dangond <silenadangond1@gmail.com>
2021-12-10 21:54:36 -0500

    union of types, aliases and literal types

--- dist/type-union.js
+++ dist/type-union.js
@@ -1,6 +1,16 @@
 "use strict";
 exports.__esModule = true;
-//los ususarios pueden venir con id 10 o con una cadenas
+//alias para un tipo de dato especifico
 var idUser;
 idUser = 10;
 idUser = '10';
+//Buscar username dado un ID
+function getUsernameById(id) {
+    ///logica de negocio, find the user
+    return "silcadc";
+}
+getUsernameById(20);
+getUsernameById('20');
+//let samllPicture: squareSize = '200x200';//error
+var samllPicture = '100x100';
+var mediunPicture = '500x500';
--- src/type-union.ts
+++ src/type-union.ts
@@ -1,5 +1,38 @@
 export {};
 //los ususarios pueden venir con id 10 o con una cadenas
-let idUser: number | string;
+// let idUser: number | string;
+// idUser = 10;
+// idUser = '10';
+
+// //Buscar username dado un ID
+// function getUsernameById(id: number | string) {
+//     ///logica de negocio, find the user
+//     return "silcadc"
+// }
+
+// getUsernameById(20);
+// getUsernameById('20');
+
+//Alias de tipos para TS
+type IdUser = number | string;
+type Username = string;//puedo establecer un
+//alias para un tipo de dato especifico
+let idUser: IdUser;
 idUser = 10;
 idUser = '10';
+
+//Buscar username dado un ID
+function getUsernameById(id: IdUser): Username {
+    ///logica de negocio, find the user
+    return "silcadc"
+}
+
+getUsernameById(20);
+getUsernameById('20');
+
+//Tipos Literales en TS
+//100x100, 500x500, 1000x1000
+type squareSize = '100x100' | '500x500' | '1000x1000';
+//let samllPicture: squareSize = '200x200';//error
+let samllPicture: squareSize = '100x100';
+let mediunPicture: squareSize = '500x500';
